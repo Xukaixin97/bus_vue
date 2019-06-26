cc9e8fc0ef203a11eb6de77e477e015bca35a5ed
Xukaixin <xmd19970616@gmail.com>
2019-06-26 10:34:12 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app">
-    <!-- <transition
-      name="custom-classes-transition"
-      enter-active-class="animated slideInRight"
-      leave-active-class="animated slideOutLeft"
-      style="animation-duration: .8s;"
-    > -->
     <!-- 自带的过度效果 -->
      <transition name="el-fade-in">
       <router-view></router-view>
